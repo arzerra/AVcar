f72ad1c2e8b7c17c4d19244651612fa615d35234
ianclark1506 <ianclark1506@gmail.com>
2024-11-11 23:55:55 +0800

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,7 @@
                         @else
                             <a href="{{ route('login') }}" class="text-white text-lg px-4 py-2 rounded-md hover:bg-gray-700 transition duration-300">
                                 Log in
-                            </a>
+                                </a>
 
                             @if (Route::has('register'))
                                 <a href="{{ route('register') }}" class="text-white text-lg px-4 py-2 rounded-md hover:bg-gray-700 transition duration-300">
@@ -65,9 +65,9 @@
 
 
       
-        <!-- <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50"> -->
-            <!-- <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" /> -->
-            <!-- <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
+        <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+            <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" />
+           <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     
 
@@ -186,6 +186,6 @@
                     </footer>
                 </div>
             </div>
-        </div> -->
+        </div>
     </body>
 </html>
